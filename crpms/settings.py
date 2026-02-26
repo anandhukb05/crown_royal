5006420490a2ba9870fcd1c971729177f8f51fea
anandhukb05 <anandhubijuabk7598@gmail.com>
2026-02-26 07:39:45 +0530

    appintment delete

--- crpms/settings.py
+++ crpms/settings.py
@@ -40,9 +40,8 @@ INSTALLED_APPS = [
     'apps.home',
     'apps.patients',
     'apps.staffs',
-    'apps.services',
-    'apps.appointment'
-]
+    'apps.services'
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -124,8 +123,11 @@ TIME_ZONE = 'Asia/Kolkata'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- crpms/urls.py
+++ crpms/urls.py
@@ -24,8 +24,8 @@ urlpatterns = [
     path('', include("apps.home.urls")),
     path('patients/', include("apps.patients.urls")),
     path('staffs/', include("apps.staffs.urls")),
-    path('services/', include("apps.services.urls")),
-    path('appointment/', include("apps.appointment.urls"))
+    path('services/', include("apps.services.urls"))
+    # path('appointment/', include("apps.appointment.urls"))
 ]
 
 if settings.DEBUG:
